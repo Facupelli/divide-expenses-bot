196b945825edbb13c343beb8ae34b2d731ddede3
Facundo Personal <facundopellicer4@gmail.com>
2025-07-22 18:31:43 -0300

    add biome format/linter

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,27 +1,26 @@
-import { insertExpenseSchema, NewExpense } from "../db/schema";
-import { SqliteExpenseRepository } from "./expense.sqlite.repository";
+import { insertExpenseSchema, type NewExpense } from "../db/schema";
+import type { SqliteExpenseRepository } from "./expense.sqlite.repository";
 
 export class ExpenseService {
-  constructor(private expenseRepository: SqliteExpenseRepository) {}
+	constructor(private expenseRepository: SqliteExpenseRepository) {}
 
-  async getAll(chatId: string) {
-    return await this.expenseRepository.getAll(chatId);
-  }
+	async getAll(chatId: string) {
+		return await this.expenseRepository.getAll(chatId);
+	}
+	async save(
+		newExpense: Omit<NewExpense, "groupId">,
+		splitBetween: string[],
+		groupId: number,
+	) {
+		const validatedExpense = insertExpenseSchema.parse({
+			...newExpense,
+			groupId,
+		});
 
-  async save(
-    newExpense: Omit<NewExpense, "groupId">,
-    splitBetween: string[],
-    groupId: number
-  ) {
-    const validatedExpense = insertExpenseSchema.parse({
-      ...newExpense,
-      groupId,
-    });
+		await this.expenseRepository.save(validatedExpense, splitBetween, groupId);
+	}
 
-    await this.expenseRepository.save(validatedExpense, splitBetween, groupId);
-  }
-
-  async getSplitBetween(expenseId: number) {
-    return await this.expenseRepository.getSplitBetween(expenseId);
-  }
+	async getSplitBetween(expenseId: number) {
+		return await this.expenseRepository.getSplitBetween(expenseId);
+	}
 }
