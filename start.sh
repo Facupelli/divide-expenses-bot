2d0b4db61744282fd0d896720dda50f33d8e084b
Facundo Personal <facundopellicer4@gmail.com>
2025-07-23 17:49:14 -0300

    update start.sh file

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
 set -e
-
 echo "Running database migrations…"
-tsx /app/src/drizzle.config.ts migrate
-
+npx drizzle-kit migrate --config=/app/src/drizzle.config.ts
 echo "Starting application…"
 exec node dist/index.js
